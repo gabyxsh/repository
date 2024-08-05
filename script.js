27209d5dfd62d03e520ffbb7ba35e46b612306d6
Angel S. <gabysh2504@gmail.com>
2024-08-05 20:26:35 +0000

    work for the class

---
+++ script.js
@@ -0,0 +1,8 @@
+let myname = "Angel"
+//console.log ("Angel")
+let wizard = "wizard"
+let mynumber = 12*12
+//console.log (mynumber)
+let adjective = "guardian"
+let twelve = "true"
+console.log ('In a rainy afternoon Angel was running trying to find a shelter, out of nowhere a random wizard spawned in front of him. He said, hello' +" Angel"+' i am the '+ adjective+' of this area, if you want shelter a true or false test you have to pass, what is the square root of 144? the wizard asked, Angel in a flash 12 replied to which the Wizard said '+ twelve+ ' and since you responded correctly, the shelter you looking for you may have.')
